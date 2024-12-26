09e583f4ae315df74fbd7681dec5c0f31be9b6fe
SiddharthCA96 <ss6156852@gmail.com>
2024-12-26 16:40:44 +0530

    created the custom hook to fetch now playing movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,23 +1,10 @@
-
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { addNowPlayingMovies } from '../utils/moviesSlice';
-const Browse = () => {
-  const dispatch=useDispatch();
-  //fcn to get recenltly/now playing movies
-  const nowPlayingMovies=async()=>{
-    const data=await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',API_OPTIONS);
-    const json=await data.json();
-   // console.log(json.results);
-    dispatch(addNowPlayingMovies({nowPlayingMovies:json.results}));
-  }
 
-  //calling the nowplayingmovies from useeffect as you know
-  useEffect(()=>{
-    nowPlayingMovies();
-  },[]); 
+const Browse = () => {
+  
+  //calling the hoook responsible to fetch nowplaying movie
+  useNowPlayingMovies();
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from black z-10'>
       <Header />
