2ecbb6a0729210326d829af344bb51b13e8737b2
SiddharthCA96 <ss6156852@gmail.com>
2024-12-29 23:19:18 +0530

    added the gptsearch component

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { LOGIN_BACK } from "../utils/constants";
+import GptMovieSuggestion from "./GptMovieSuggestion";
+import GptSearchBar from "./GptSearchBar";
 
-const GptSearch = () => {
+const GPTSearch = () => {
   return (
-    <div>
-        this is gpt search
-    </div>
-  )
-}
-
-export default GptSearch
+    <>
+      <div className="fixed -z-10">
+        <img className="h-screen w-screen object-cover" src={LOGIN_BACK} alt="logo" />
+      </div>
+      <div className="">
+        <GptSearchBar />
+        <GptMovieSuggestion />
+      </div>
+    </>
+  );
+};
+export default GPTSearch;
